106c9f0b29bef948437e191ae6beaa753e6bd2bb
Muhammad Ridwan Fathin <Muhammad Ridwan Fathin>
2016-05-04 17:52:35 +0700

    base url fixed

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost:1717/msp_easyorder/';
+$config['base_url'] = 'http://easyorderfathin.azurewebsites.net/';
 
 /*
 |--------------------------------------------------------------------------
